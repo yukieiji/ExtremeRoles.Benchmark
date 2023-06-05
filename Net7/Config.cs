5ad01430f4d23f2f16650d4a300375ee119a6f3a
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-06-06 02:47:51 +0900

    feat:memory

--- Net7/Config.cs
+++ Net7/Config.cs
@@ -10,7 +10,7 @@ public class BenchmarkConfig : ManualConfig
     public BenchmarkConfig()
     {
         AddExporter(MarkdownExporter.GitHub); // ベンチマーク結果を書く時に出力させとくとベンリ
-        // Add(MemoryDiagnoser.Default);
+        AddDiagnoser(MemoryDiagnoser.Default);
 
         // ShortRunを使うとサクッと終わらせられる、デフォルトだと本気で長いので短めにしとく。
         // ShortRunは LaunchCount=1  TargetCount=3 WarmupCount = 3 のショートカット
